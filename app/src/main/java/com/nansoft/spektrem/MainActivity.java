eb3f0b3ae17fc367f7a08d750229e1d87ec1e62e
Carlos Castro Brenes <ccasbre27@hotmail.com>
2016-04-06 19:32:25 -0600

    startToTalk function implemented

--- app/src/main/java/com/nansoft/spektrem/MainActivity.java
+++ app/src/main/java/com/nansoft/spektrem/MainActivity.java
@@ -1,13 +1,58 @@
 package com.nansoft.spektrem;
 
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    EditText edtUserText;
+    private TextToSpeech mTts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // referencias al edit text
+        edtUserText = (EditText) findViewById(R.id.edtUserText);
+
+
+    }
+
+
+
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            case R.id.btnStartTalk:
+                startToTalk();
+                break;
+
+            case R.id.btnShareText:
+                break;
+
+            case R.id.btnReset:
+                break;
+        }
+    }
+
+    private void startToTalk()
+    {
+        TextToSpeech textToSpeech = new TextToSpeech(this, this);
+        textToSpeech.speak(edtUserText.getText().toString(),TextToSpeech.QUEUE_FLUSH, null);
+
+    }
+
+    // listener tts
+    @Override
+    public void onInit(int i) {
+
     }
 }
